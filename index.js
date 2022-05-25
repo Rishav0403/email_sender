be571fcd91221eac81fd6a0719c912f05ddf8fde
Rishav0403 <barnwalrishav82@gmail.com>
2022-05-25 11:19:11 +0530

    v1.7

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ app.post('/api/v1/sendmail', upload, (req, res) => {
 
 app.use(express.static("client/build"));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
--- server.js
+++
@@ -1,8 +0,0 @@
-const xlsx = require('xlsx');
-const wb = xlsx.readFile("./uploads/f.xlsx", {cellDates: true});
-// console.log(wb.SheetNames)
-
-const ws = wb.Sheets["Sheet1"];
-// console.log(ws)
-const data = xlsx.utils.sheet_to_json(ws);
-console.log(data)
